a2859b84a896cabc712cbbd43ff0ca8cb8d57bed
dccontreras50 <73891538+dccontreras50@users.noreply.github.com>
2023-02-06 19:42:55 -0500

    commit inicial

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function copiartexto(){
-    alert ("No se ha podido copiar tu hermoso texto, dale atras en el navegador para recuperarlo");
-}
+    const $content = document.getElementById('cuadrodetexto'),
+        $btncopy = document.getElementById('btncopy'),
+        $title = document.getElementById('title');
+
+$btncopy.addEventListener('click', e =>{
+    $content.select();
+    document.execCommand('copy');
+
+    $title.innerHTML = 'Copiado!'
+})
