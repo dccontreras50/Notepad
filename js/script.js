ee64869b18f5600b4ca7c4d6535cf891dfc461a3
dccontreras50 <73891538+dccontreras50@users.noreply.github.com>
2023-06-30 14:14:53 -0500

    commit inicial

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ $btncopy.addEventListener('click', e =>{
 
     $title.innerHTML = 'Copiado!'
 })
+
+var switchButton = document.getElementById("switch-button");
+var myDiv = document.getElementById("agrandarcuadrotxt");
+
+switchButton.addEventListener("click", function() {
+  if (myDiv.classList.contains("col-lg-5")) {
+    myDiv.classList.remove("col-lg-5");
+    myDiv.classList.add("col-lg-12");
+  } else {
+    myDiv.classList.remove("col-lg-12");
+    myDiv.classList.add("col-lg-5");
+  }
+});
